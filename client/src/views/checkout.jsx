57943abd3dc6d0bfee7519486eee5b9cdc37ddf1
Ben Baldwin <baldwin.ben55@gmail.com>
2023-02-22 17:04:44 -0800

    modularized the checkout card

--- client/src/views/checkout.jsx
+++ client/src/views/checkout.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import CheckoutForm from '../components/CheckoutComponents/CheckoutForm'
 import StripePaymentForm from '../components/CheckoutComponents/StripePaymentForm'
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
   return (
     <div className='bg-gradient-to-br from-slate-50 to-stone-300'>
-      <CheckoutForm />
+      <CheckoutForm  cart={cart}/>
         {/* <StripePaymentForm /> */}
     </div>
   )
