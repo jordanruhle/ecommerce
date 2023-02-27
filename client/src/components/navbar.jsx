1256fa4fa092387f039f944841087fecaa847f3b
Peter Disney <peter.w.disney@gmail.com>
2023-02-27 09:43:26 -0800

    Modularized company logo and added routing

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,17 +1,16 @@
+import React from 'react'
 import { Fragment } from 'react'
 import { Transition, Menu } from '@headlessui/react'
-import { FaShoppingCart, FaMountain } from 'react-icons/fa'
+import { FaShoppingCart } from 'react-icons/fa'
 
-import React from 'react'
+import CompanyLogo from './CompanyLogo'
 
 const Navbar = () => {
     return (
         <div className='w-full flex justify-center sm:justify-between items-center py-2 px-8  text-white bg-stone-800 font-body flex-wrap'>
 
             {/*----------- Company Logo ------------ */}
-
-            <h1 className='text-2xl font-bold bg-white rounded-full border-slate-300 border-8 px-2 text-stone-800 min-w-max' > Mountain <FaMountain className='mb-1' style={{display:"inline-block"}} /> Bikes</h1>
-
+            <CompanyLogo linkRoute='/'/>
             {/* ------------- Links ----------------- */}
 
             <ul className='flex items-center '>
