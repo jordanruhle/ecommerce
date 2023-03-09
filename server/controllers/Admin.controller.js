192ccd7a0a508e7c6e2c551bb11b7f07ca1f5f29
Ben Baldwin <baldwin.ben55@gmail.com>
2023-03-09 15:55:55 -0800

    added admin login controller functionality

--- server/controllers/Admin.controller.js
+++ server/controllers/Admin.controller.js
@@ -1,19 +1,20 @@
 const Admins = require("../models/Admin.model")
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 
 // Read All
 module.exports.getAllAdmins = (req, res) => {
   Admins.find()
-      .then(allAdmins => res.json({ admin: allAdmins }))
-      .catch(err => res.json({ message: 'Something went wrong', error: err }));
+    .then(allAdmins => res.json({ admin: allAdmins }))
+    .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
 
 // Find One
 module.exports.findoneSingleAdmin = (req, res) => {
   Admins.findOne({ _id: req.params.id })
-      .then(oneSingleAdmin => res.json({ admin: oneSingleAdmin }))
-      .catch(err => res.json({ message: 'Something went wrong', error: err }));
+    .then(oneSingleAdmin => res.json({ admin: oneSingleAdmin }))
+    .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
 
 // Create One
@@ -24,27 +25,47 @@ module.exports.createNewAdmin = async (req, res) => {
     await newAdmin.save()
     console.log(newAdmin)
     const payload = {
-          id: newAdmin._id
-        }
-        const adminToken = jwt.sign(payload, process.env.ADMIN_LOGIN_REG_SECRET_KEY)
-        console.log(adminToken);
-        res.json({ msg: "success!", admin: admin, adminToken: adminToken });
-  } catch(err) {
+      id: newAdmin._id
+    }
+    const adminToken = jwt.sign(payload, process.env.ADMIN_LOGIN_REG_SECRET_KEY)
+    console.log(adminToken);
+    res.json({ msg: "success!", admin: admin, adminToken: adminToken });
+  } catch (err) {
     res.json(err)
-  } 
+  }
 }
 
 // variable = function for login
-// takes request = email + password
-// checks if email is in the Database
-// Admin.findOne({ username: { $regex: /^john$/i } }, function(err, admin) {
-//   // ...
-// });
-// checks if password associated with email matches given password
-// 
-// Build Response
-// create JWT
-// respond message + JWT
+module.exports.adminLogin = async (req, res) => {
+  console.log("got to admin login");
+  console.log(req.body);
+  // takes request = email + password
+  const { email, password } = req.body;
+  try {
+    // checks if email is in the Database
+    const admin = await Admins.findOne({ email: email })
 
-// catch if the shit above breaks
-// send err and error message
+    const matchedPasswords = await bcrypt.compare(password, admin.password)
+
+    console.log(admin.password);
+    console.log(password);
+    // checks if password associated with email matches given password
+    if (matchedPasswords) {
+      console.log("made it to the if");
+      // Build Response
+      const payload = {
+        id: admin._id
+      }
+      // create JWT
+      const adminToken = jwt.sign(payload, process.env.ADMIN_LOGIN_REG_SECRET_KEY)
+      console.log(adminToken);
+      // respond message + JWT
+      res.json({ msg: "login succesful", adminToken: adminToken })
+    }
+  } catch (err) {
+    console.log(err);
+    // catch if the shit above breaks
+    // send err and error message
+    res.json(err)
+  }
+}
