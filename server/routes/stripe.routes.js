75ac8bbc180b298fcddfc279fb471a799646d101
Jordan Ruhle <jordanruhle@gmail.com>
2023-03-28 11:02:51 -0700

    added api to charge route

--- server/routes/stripe.routes.js
+++ server/routes/stripe.routes.js
@@ -2,7 +2,7 @@ const StripeController = require('../controllers/stripe.controller');
 
 
 module.exports = app => {
-    app.get("/stripe/payment", StripeController.payment);
-    app.get("/stripe/config", StripeController.config);
-    app.post('/stripe/charge', StripeController.createPaymentIntent);
+    app.get("/api/stripe/payment", StripeController.payment);
+    app.get("/api/stripe/config", StripeController.config);
+    app.post('/api/stripe/charge', StripeController.createPaymentIntent);
 }
