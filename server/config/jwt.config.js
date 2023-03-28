bf87361467e6add88a40c5258b05b8c9cffc43a8
Jordan Ruhle <jordanruhle@gmail.com>
2023-03-27 17:14:28 -0700

    fixing routes and addin logs

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -2,9 +2,11 @@ const jwt = require("jsonwebtoken");
  
 module.exports.authenticate =async (req, res, next) => {
   const adminToken = req.cookies.adminToken;
+  console.log("admin token: " + adminToken)
   try {
     const decoded = jwt.verify(adminToken, process.env.ADMIN_LOGIN_REG_SECRET_KEY)
     const adminId = decoded.id
+    console.log("admin id: " + adminId)
     res.json({verified: true})
   } catch (err) {
     res.json('401 You are unauthorized!')
